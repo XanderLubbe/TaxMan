92b8c94783b96b869f887c6f99a7df5ee597a79f
Xander Lubbe <xlubbe@gmail.com>
2022-06-20 10:46:21 +0200

    Added my project

---
+++ src/main/java/com/xanderlubbe/taxman/TaxManApplication.java
@@ -0,0 +1,13 @@
+package com.xanderlubbe.taxman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxManApplication.class, args);
+	}
+
+}
