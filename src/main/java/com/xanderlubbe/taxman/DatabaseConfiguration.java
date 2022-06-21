48b497df47f8335461587b24a27edbd871ef2fe1
Xander Lubbe <xlubbe@gmail.com>
2022-06-21 13:22:46 +0200

    Data and DatabaseConfig have been set

--- src/main/java/com/xanderlubbe/taxman/DatabaseConfiguration.java
+++ src/main/java/com/xanderlubbe/taxman/DatabaseConfiguration.java
@@ -10,6 +10,8 @@ import org.springframework.core.io.ClassPathResource;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,30 +24,41 @@ class DatabaseConfiguration {
 
         return args -> {
 
-            ClassPathResource classPathResource = new ClassPathResource("data.csv");
-            InputStreamReader inputStreamReader = new InputStreamReader(classPathResource.getInputStream());
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            String line = bufferedReader.readLine();
 
-            while (line != null){
-                System.out.println(line);
-                String[] cols = line.split(", ");
-                int size = cols.length;
-                int[] intArr = new int[size];
-                for(int i=0; i<size; i++) {
-                    intArr[i] = Integer.parseInt(cols[i]);
+            File f = new File("/Users/xander/Desktop/Projects/TaxMan/src/main/resources/data.sql");
+            if(f.exists()) {
+                System.out.println("fileName exists");
+            } else {
+                System.out.println("fileName does not exist");
+                ClassPathResource classPathResource = new ClassPathResource("data.csv");
+                InputStreamReader inputStreamReader = new InputStreamReader(classPathResource.getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = bufferedReader.readLine();
+
+                while (line != null){
+                    System.out.println(line);
+                    String[] cols = line.split(", ");
+                    int size = cols.length;
+                    int[] intArr = new int[size];
+                    for(int i=0; i<size; i++) {
+                        intArr[i] = Integer.parseInt(cols[i]);
+                    }
+
+                    repository.save(new Entry(intArr[0], intArr[1], intArr[2], intArr[3], intArr[4]));
+                    line = bufferedReader.readLine();
                 }
 
-                repository.save(new Entry(intArr[0], intArr[1], intArr[2], intArr[3], intArr[4]));
-                line = bufferedReader.readLine();
+                bufferedReader.close();
+
+                // TODO generate backup sql file here
             }
 
-            bufferedReader.close();
 
 
 
-            log.info("Preloading " + repository.save(new Entry(0,1,2,3,4)));
-            log.info("Preloading " + repository.save(new Entry(10,9,8,7,6)));
+
+//            log.info("Preloading " + repository.save(new Entry(00,11,22,33,44)));
+//            log.info("Preloading " + repository.save(new Entry(10,9,8,7,6)));
         };
     }
 }
