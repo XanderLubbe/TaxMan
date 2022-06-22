e11148a3e1c15056181455a1d47288434a81765d
Xander Lubbe <x.lubbe@icloud.com>
2022-06-22 11:45:19 +0200

    Created taxService to handle movement between the controller and repo

--- src/main/java/com/xanderlubbe/taxman/controller/TaxController.java
+++ src/main/java/com/xanderlubbe/taxman/controller/TaxController.java
@@ -2,6 +2,7 @@ package com.xanderlubbe.taxman.controller;
 //
 import com.xanderlubbe.taxman.model.Tax;
 import com.xanderlubbe.taxman.repository.TaxRepository;
+import com.xanderlubbe.taxman.service.TaxService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +11,21 @@ import java.util.Collection;
 
 @RestController
 public class TaxController {
-    private final TaxRepository repository;
+    private final TaxService service;
 
-    TaxController(TaxRepository repository) {
-
-        this.repository = repository;
+    TaxController(TaxService service) {
+        this.service = service;
     }
 
     // Get all entries in the database currently
     @GetMapping("/tax")
     Collection<Tax> findTaxes(@RequestParam int salary, int age) {
-        return repository.findTaxes(salary);
+        return service.findTaxesService(salary, age);
     }
 
     @GetMapping("/taxes")
     Collection<Tax> findTaxes(@RequestParam int salary) {
-        return repository.findTaxes(salary);
+        return service.findTaxesService(salary);
     }
 
 }
--- src/main/java/com/xanderlubbe/taxman/service/TaxService.java
+++ src/main/java/com/xanderlubbe/taxman/service/TaxService.java
@@ -1,4 +1,26 @@
 package com.xanderlubbe.taxman.service;
 
+import com.xanderlubbe.taxman.model.Tax;
+import com.xanderlubbe.taxman.repository.TaxRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
 public class TaxService {
+
+    private final TaxRepository repository;
+
+    TaxService(TaxRepository repository) {
+        this.repository = repository;
+    }
+
+    public Collection<Tax> findTaxesService(int salary){
+        return  repository.findTaxesRepo(salary);
+    }
+
+    public Collection<Tax> findTaxesService(int salary, int age){
+        return  repository.findTaxesRepo(salary);
+    }
 }
