26eb374b74f342f5f2234feebb5c3f16bb36914e
Xander Lubbe <x.lubbe@icloud.com>
2022-06-22 13:37:11 +0200

    Repository updated

--- src/main/java/com/xanderlubbe/taxman/repository/TaxRepository.java
+++ src/main/java/com/xanderlubbe/taxman/repository/TaxRepository.java
@@ -1,17 +1,15 @@
 package com.xanderlubbe.taxman.repository;
 
-import com.xanderlubbe.taxman.model.Tax;
+import com.xanderlubbe.taxman.model.TaxDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.Collection;
-
 import static com.xanderlubbe.taxman.Constants.TAX_TABLE_NAME;
 
-public interface TaxRepository extends JpaRepository<Tax, Long> {
+public interface TaxRepository extends JpaRepository<TaxDTO, Long> {
 
       @Query("SELECT e FROM " + TAX_TABLE_NAME + " e WHERE e.lowerLimit <= :salary AND e.upperLimit >= :salary")
-      Collection<Tax> findTaxesRepo(@Param("salary") int salary);
+      TaxDTO findTaxesRepo(@Param("salary") int salary);
 
 }
